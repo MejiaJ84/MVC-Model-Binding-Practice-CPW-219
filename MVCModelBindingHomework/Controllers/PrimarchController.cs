24a731b5a55c6137f6faf1c4a80c109ad339c999
MejiaJ84 <jtmejia1984@gmail.com>
2022-01-20 17:28:25 -0800

    Add project files.

---
+++ MVCModelBindingHomework/Controllers/PrimarchController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCModelBindingHomework.Models;
+
+namespace MVCModelBindingHomework.Controllers
+{
+    public class PrimarchController : Controller
+    {
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult CreateWithModelBinding()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCModelBindingHomework/Models/Primarch.cs
@@ -0,0 +1,26 @@
+﻿namespace MVCModelBindingHomework.Models
+{
+    public class Primarch
+    {
+        /// <summary>
+        /// Full Name of the Primarch
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Order in which the Primarch was created
+        /// </summary>
+        public int PrimarchNumber { get; set; }
+
+        /// <summary>
+        /// The legion that belongs to the Primarch
+        /// </summary>
+        public string Legion { get; set; }
+
+        /// <summary>
+        /// The planer where each Primarch was found
+        /// and which he considers home
+        /// </summary>
+        public string HomeWorld { get; set; }
+    }
+}
